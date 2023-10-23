b2548ee223ab89ae332a083602612ae8b6a21554
Juan Carlos <juanvillarq0097@gmail.com>
2023-10-23 11:50:29 -0500

    Creacion de Clases controladoras jpa para ususario y horario,creacion de constructor para crear una instancias de administracion para las entidades del proyecto apuntando al archivo de persistencia

--- target/generated-sources/annotations/com/back/model/Usuario_.java
+++ target/generated-sources/annotations/com/back/model/Usuario_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-10-23T11:37:29")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-10-23T11:47:00")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
